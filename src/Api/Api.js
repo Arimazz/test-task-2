d200e96f515ea1c65dfd2b3bfd872a290f10b778
Arimazz <bodyaprymyak@icloud.com>
2020-03-23 02:57:05 +0200

    http to https request fixed

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,5 @@
 const API_KEY = '4891b23ccb9c451d9519eb7d09812fdd';
-const url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
 export const getNews = async() => {
   const response = await fetch(url);
